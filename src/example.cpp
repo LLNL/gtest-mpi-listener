6d8b97cecd41682308647000f2158933817893ac
Geoffrey Oxberry <oxberry1@llnl.gov>
2016-11-22 19:58:02 -0800

    Add first working version of MPI EventListener
    Add a first working version of an MPI-capable EventListener. An example
    test runner with a `main()` function is also provided.

---
+++ src/example.cpp
@@ -0,0 +1,86 @@
+#include "gtest/gtest.h"
+#include "gtest-mpi-listener.hpp"
+#include "mpi.h"
+
+// Simple-minded functions for some testing
+
+// Always passes out == rank
+int getMpiRank(MPI_Comm comm) {
+  int out;
+  MPI_Comm_rank(comm, &out);
+  return out;
+}
+
+// Always fails out == rank
+int getMpiRankPlusOne(MPI_Comm comm) {
+  int out;
+  MPI_Comm_rank(comm, &out);
+  return (out+1);
+}
+
+// Passes out == rank when rank is zero, fails otherwise
+int getZero(MPI_Comm comm) {
+  return 0;
+}
+
+// Passes out == rank except on rank zero, fails otherwise
+int getNonzeroMpiRank(MPI_Comm comm) {
+  int out;
+  MPI_Comm_rank(comm, &out);
+  return (out ? out : 1);
+}
+
+// These tests could be made shorter with a fixture, but a fixture
+// deliberately isn't used in order to make the test harness extremely simple
+TEST(BasicMPI, PassOnAllRanks) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank;
+  MPI_Comm_rank(comm, &rank);
+  EXPECT_EQ(rank, getMpiRank(comm));
+}
+
+TEST(BasicMPI, FailOnAllRanks) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank;
+  MPI_Comm_rank(comm, &rank);
+  EXPECT_EQ(rank, getMpiRankPlusOne(comm));
+}
+
+TEST(BasicMPI, FailExceptOnRankZero) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank;
+  MPI_Comm_rank(comm, &rank);
+  EXPECT_EQ(rank, getZero(comm));
+}
+
+TEST(BasicMPI, PassExceptOnRankZero) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank;
+  MPI_Comm_rank(comm, &rank);
+  EXPECT_EQ(rank, getNonzeroMpiRank(comm));
+}
+
+int main(int argc, char** argv) {
+  // Filter out googletest arguments
+  ::testing::InitGoogleTest(&argc, argv);
+
+  MPI_Init(&argc, &argv);
+
+  ::testing::AddGlobalTestEnvironment(new MPIEnvironment);
+
+  // Get the event listener list.
+  ::testing::TestEventListeners& listeners =
+      ::testing::UnitTest::GetInstance()->listeners();
+
+  // Remove default listener
+  delete listeners.Release(listeners.default_result_printer());
+
+  // Adds MPI listener; Google Test owns this pointer
+  listeners.Append(new MPIMinimalistPrinter);
+
+  // Run tests, then clean up and exit
+  RUN_ALL_TESTS();
+
+  //  MPI_Finalize();
+  return 0;
+}
